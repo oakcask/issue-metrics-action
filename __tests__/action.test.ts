6f548041746c3b68fd56be78907dddcb62e378df
Kuya KOHARA <kuya.kohara@gmail.com>
2025-10-30 01:21:11 +0900

    style: format

--- __tests__/action.test.ts
+++ __tests__/action.test.ts
@@ -1,80 +1,85 @@
 /* eslint-disable camelcase */
-import { describe, expect, it, beforeEach } from 'vitest'
-import { generateMetrics, parseParameters } from '../src/action'
+import { beforeEach, describe, expect, it } from 'vitest';
+import { generateMetrics, parseParameters } from '../src/action';
 
 describe('parseParameters', () => {
   beforeEach(() => {
-    process.env.GITHUB_REPOSITORY = 'foo/bar'
-    process.env.GITHUB_EVENT_PATH = '__tests__/fixtures/payload.json'
-  })
+    process.env.GITHUB_REPOSITORY = 'foo/bar';
+    process.env.GITHUB_EVENT_PATH = '__tests__/fixtures/payload.json';
+  });
 
   it('takes owner/repo from env', async () => {
-    const params = await parseParameters()
-    expect(params?.owner).toEqual('foo')
-    expect(params?.repo).toEqual('bar')
-  })
+    const params = await parseParameters();
+    expect(params?.owner).toEqual('foo');
+    expect(params?.repo).toEqual('bar');
+  });
 
   it('takes owner/repo from payload instead if the env is empty', async () => {
-    delete process.env.GITHUB_REPOSITORY
-    const params = await parseParameters()
-    expect(params?.owner).toEqual('owner-in-payload')
-    expect(params?.repo).toEqual('repo-in-payload')
-  })
+    delete process.env.GITHUB_REPOSITORY;
+    const params = await parseParameters();
+    expect(params?.owner).toEqual('owner-in-payload');
+    expect(params?.repo).toEqual('repo-in-payload');
+  });
 
   it('takes issue number and body from payload', async () => {
-    const params = (await parseParameters())!
-    expect(params.number).toEqual(42)
-  })
+    const params = (await parseParameters())!;
+    expect(params.number).toEqual(42);
+  });
 
   it('takes timestamp', async () => {
-    const params = (await parseParameters())!
-    expect(params.createdAt).toEqual('2006-01-02T15:04:06+0700')
-  })
+    const params = (await parseParameters())!;
+    expect(params.createdAt).toEqual('2006-01-02T15:04:06+0700');
+  });
 
   describe('when the payload withoout a issue', async () => {
     beforeEach(() => {
-      process.env.GITHUB_EVENT_PATH = '__tests__/fixtures/payload.no-issue.json'
-    })
+      process.env.GITHUB_EVENT_PATH =
+        '__tests__/fixtures/payload.no-issue.json';
+    });
 
     it('is undefined', async () => {
-      const params = await parseParameters()
-      expect(params).toBeUndefined()
-    })
-  })
+      const params = await parseParameters();
+      expect(params).toBeUndefined();
+    });
+  });
 
   describe('when the payload includes pull_request', () => {
     beforeEach(() => {
-      process.env.GITHUB_EVENT_PATH = '__tests__/fixtures/payload.pull_request.json'
-    })
+      process.env.GITHUB_EVENT_PATH =
+        '__tests__/fixtures/payload.pull_request.json';
+    });
 
     it('takes pull request number and body from payload', async () => {
-      const params = (await parseParameters())!
-      expect(params.number).toEqual(4242)
-      expect('merged' in params && params.merged).toEqual(true)
-    })
+      const params = (await parseParameters())!;
+      expect(params.number).toEqual(4242);
+      expect('merged' in params && params.merged).toEqual(true);
+    });
 
     it('takes timestamp', async () => {
-      const params = (await parseParameters())!
-      expect(params.createdAt).toEqual('2006-01-02T15:04:06+0700')
-      expect(params.closedAt).toEqual('2016-01-02T15:04:06+0700')
-    })
-  })
-})
+      const params = (await parseParameters())!;
+      expect(params.createdAt).toEqual('2006-01-02T15:04:06+0700');
+      expect(params.closedAt).toEqual('2016-01-02T15:04:06+0700');
+    });
+  });
+});
 
 describe('generateMetrics', () => {
   it('generates issue metrics', () => {
-    const got = generateMetrics({
-      owner: 'foo',
-      repo: 'bar',
-      type: 'issue',
-      action: 'opened',
-      number: 42,
-      labels: [{ name: 'feature' }, { name: 'needs:triage' }],
-      createdAt: '2006-01-02T15:04:06+0700'
-    }, {
-      epic: true,
-      'needs:triage': true
-    })
+    const got = generateMetrics(
+      {
+        owner: 'foo',
+        repo: 'bar',
+        type: 'issue',
+        action: 'opened',
+        number: 42,
+        labels: [{ name: 'feature' }, { name: 'needs:triage' }],
+        createdAt: '2006-01-02T15:04:06+0700',
+      },
+      {
+        epic: true,
+        'needs:triage': true,
+      },
+    );
 
     const activities_count = {
       type: 'count',
@@ -84,28 +89,31 @@ describe('generateMetrics', () => {
         is: 'issue',
         number: '42',
         action: 'opened',
-        'needs:triage': true
+        'needs:triage': true,
       },
-      value: 1
-    }
-    expect(got).toStrictEqual({ activities_count })
-  })
+      value: 1,
+    };
+    expect(got).toStrictEqual({ activities_count });
+  });
 
   it('generates PR metrics', () => {
-    const got = generateMetrics({
-      owner: 'foo',
-      repo: 'bar',
-      type: 'pr',
-      action: 'closed',
-      number: 42,
-      labels: [{ name: 'bugfix' }, { name: 'techdebt' }],
-      createdAt: '2006-01-02T15:04:06+0700',
-      closedAt: '2006-01-02T15:14:06+0700',
-      merged: true
-    }, {
-      techdebt: true,
-      bug: true
-    })
+    const got = generateMetrics(
+      {
+        owner: 'foo',
+        repo: 'bar',
+        type: 'pr',
+        action: 'closed',
+        number: 42,
+        labels: [{ name: 'bugfix' }, { name: 'techdebt' }],
+        createdAt: '2006-01-02T15:04:06+0700',
+        closedAt: '2006-01-02T15:14:06+0700',
+        merged: true,
+      },
+      {
+        techdebt: true,
+        bug: true,
+      },
+    );
 
     const activities_count = {
       type: 'count',
@@ -116,10 +124,10 @@ describe('generateMetrics', () => {
         number: '42',
         action: 'closed',
         merged: true,
-        techdebt: true
+        techdebt: true,
       },
-      value: 1
-    }
+      value: 1,
+    };
     const duration_seconds = {
       type: 'count',
       name: 'duration_seconds',
@@ -128,10 +136,10 @@ describe('generateMetrics', () => {
         is: 'pr',
         number: '42',
         merged: true,
-        techdebt: true
+        techdebt: true,
       },
-      value: 600
-    }
-    expect(got).toStrictEqual({ activities_count, duration_seconds })
-  })
-})
+      value: 600,
+    };
+    expect(got).toStrictEqual({ activities_count, duration_seconds });
+  });
+});
--- __tests__/template.test.ts
+++ __tests__/template.test.ts
@@ -1,51 +1,55 @@
-import { describe, expect, it } from 'vitest'
-import Template, { ddtags, statsdType } from '../src/template'
-import { Metric } from '../src/metrics'
+import { describe, expect, it } from 'vitest';
+import type { Metric } from '../src/metrics';
+import Template, { ddtags, statsdType } from '../src/template';
 
 describe('statsdType', () => {
   it('returns c if count given', () => {
-    expect(statsdType('count')).toEqual('c')
-  })
+    expect(statsdType('count')).toEqual('c');
+  });
   it('returns g if gauge given', () => {
-    expect(statsdType('gauge')).toEqual('g')
-  })
+    expect(statsdType('gauge')).toEqual('g');
+  });
   it('return nothing if otherwise', () => {
-    expect(statsdType('rate')).toEqual('')
-  })
-})
+    expect(statsdType('rate')).toEqual('');
+  });
+});
 
 describe('ddtags', () => {
   it('serializes tags', () => {
-    expect(ddtags({
-      one: '42',
-      two: true,
-      three: 42 // value will be removed because not string
-    })).toEqual('one:42,two,three')
-  })
+    expect(
+      ddtags({
+        one: '42',
+        two: true,
+        three: 42, // value will be removed because not string
+      }),
+    ).toEqual('one:42,two,three');
+  });
   it('removes , and |', () => {
-    expect(ddtags({
-      one: '1,2',
-      't|wo': '3,4'
-    })).toEqual('one:12,two:34')
-  })
+    expect(
+      ddtags({
+        one: '1,2',
+        't|wo': '3,4',
+      }),
+    ).toEqual('one:12,two:34');
+  });
   it('preserves already serialized tags', () => {
     const given = {
       one: '42',
-      two: true
-    }
-    expect(ddtags(ddtags(given))).toEqual(ddtags(given))
-  })
-})
+      two: true,
+    };
+    expect(ddtags(ddtags(given))).toEqual(ddtags(given));
+  });
+});
 
 describe('example', () => {
   it('example to generate dd-statsd', () => {
     const templ = `{{#each metrics}}{{name}}:{{value}}|{{statsdType type}}|{{ddtags tags}}
-{{/each}}`
-    const t = Template.compile(templ)
+{{/each}}`;
+    const t = Template.compile(templ);
     const metrics: Metric[] = [
       { type: 'count', name: 'c1', value: 42, tags: { t: '3' } },
-      { type: 'gauge', name: 'g1', value: 4242, tags: { t: '33' } }
-    ]
-    expect(t({ metrics })).toEqual('c1:42|c|t:3\ng1:4242|g|t:33\n')
-  })
-})
+      { type: 'gauge', name: 'g1', value: 4242, tags: { t: '33' } },
+    ];
+    expect(t({ metrics })).toEqual('c1:42|c|t:3\ng1:4242|g|t:33\n');
+  });
+});
